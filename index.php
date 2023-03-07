81f30eaf9855abefde6f6e1696793e3f2c4cdfa5
Desinic8 <125421310+Desinic8@users.noreply.github.com>
2023-03-07 08:57:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
     PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
   );
 
-  $pdo = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);
+  $pdo = new PDO($dsn, $_ENV["56y8i56cghatvqogls00"], $_ENV["pscale_pw_uiP98VNiSdL0HPLI5fXfZdX1wdwSduD9dveiOBArU73"], $options);
 ?>
